f6ca03f22e77dd67c80b2c5713c3804f7bbb4124
grozny <h6umid@gmail.com>
2023-03-27 12:24:34 +0300

    настройка input и кнопка добавления

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(1)
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const ul = document.getElementById('ul')
+
+form.addEventListener('submit', (eventObject) => {
+    eventObject.preventDefault()
+    const li = document.createElement('li')
+    li.textContent = input.value;
+    ul.append(li)
+    input.value = '';
+})
