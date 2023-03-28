7f5cb4268e897f0a8772315d5ed4682d0c927b18
grozny <h6umid@gmail.com>
2023-03-28 17:38:10 +0300

    настройка кнопки - удаления элементов

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
-const ul = document.getElementById('ul')
+const ul = document.createElement('ul')
+form.after(ul) 
 
 form.addEventListener('submit', (eventObject) => {
     eventObject.preventDefault()
+
     const li = document.createElement('li')
     li.textContent = input.value;
-    ul.append(li)
+
+    const dlt = document.createElement('button')
+    dlt.textContent = 'x'
+
+    dlt.addEventListener('click', (eventObject) => {
+        eventObject.target.parentElement.remove()
+    })
+
+    const mark = document.createElement('input')
+    mark.setAttribute('type', 'checkbox')
+
+    ul.prepend(mark)
+    li.append(dlt)
+    ul.prepend(li)
     input.value = '';
 })
